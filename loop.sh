b1d0feb6668b525083e768c36a84a5e50b9f2a74
mohit <85410744+mohit191120@users.noreply.github.com>
2021-10-22 00:36:15 +0530

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -5,7 +5,7 @@ git config --global user.email "sudhiryadav.igi@gmail.com"
 
 git clone https://github.com/mohit191120/Builder-CI loop
 cd loop || exit 1
-echo $(( RANDOM - ( RANDOM % RANDOM ) )) > build.txt
-git add looper.txt
+echo $(( RANDOM - ( RANDOM % RANDOM ) )) > builds.txt
+git add builds.txt
 git commit -m "Build : Loop"
 git push -q https://${TOKEN}@github.com/mohit191120/Builder-CI HEAD:sakura
